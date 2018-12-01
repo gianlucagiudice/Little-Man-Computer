b71a45049cd2c003ec04dec705ef700427e2fd4b
gianlucagiudice <gianlu.giudice@gmail.com>
2018-12-01 22:22:25 +0100

    Assembler/3 Compile error message and more

--- little_man_computer.pl
+++ little_man_computer.pl
@@ -21,31 +21,28 @@ guitracer
 
 /*
 instruction/3: List of instructions (word reserved).
-First argument  = reserved word for instruction.
-Second argument = machine code relative to the instruction.
-Third argument  = argument of the instruction
+First   argument = reserved word for instruction.
+Second  argument = machine code relative to the instruction.
+Third   argument = argument of the instruction
+Fourth  argument = Current
 */
 
 /*
-Forse potrei utilizzare "_" anzichè X. Questo perchè a me serve
-sapere se una particolare istruzione ammette o no argomenti,
-non mi interessa l'argomento nello specifico.
-
 Tutte queste istruzioni hannno una implicazione ovvero la loro esecuzione
 */
-instruction(dat, 0, Arg, State).
-instruction(add, 1, Arg, State).
-instruction(sub, 2, Arg, State).
-instruction(sta, 3, Arg, State).
-instruction(lda, 5, Arg, State).
-instruction(bra, 6, Arg, State).
-instruction(brz, 7, Arg, State).
-instruction(brp, 8, Arg, State).
-instruction(dat, 0, Arg, State).
-instruction(dat, 0, Arg, State).
-instruction(hlt, 0, Arg, State).
-instruction(inp, 901, Arg, State).
-instruction(out, 902, Arg, State).
+instruction(dat, 0, Arg, State, NewState).
+instruction(add, 1, Arg, State, NewState).
+instruction(sub, 2, Arg, State, NewState).
+instruction(sta, 3, Arg, State, NewState).
+instruction(lda, 5, Arg, State, NewState).
+instruction(bra, 6, Arg, State, NewState).
+instruction(brz, 7, Arg, State, NewState).
+instruction(brp, 8, Arg, State, NewState).
+instruction(dat, 0  , State, NewState).
+instruction(dat, 0  , State, NewState).
+instruction(hlt, 0  , State, NewState).
+instruction(inp, 901, State, NewState).
+instruction(out, 902, State, NewState).
 % explode state ritorna una lista contenente gli argomenti di state()
 
 
@@ -91,54 +88,35 @@ Devo guardare la lunghezza delle liste relative alle righe:
 
 %%% evaluate_line/3: Given a list of string codes, get the first line
 
-is_white(C) :- char_type(C, white).
 
-
-%%% skip_whitespaces/2
-%%% skip_whitespaces(Input, MoreInput)
+%%% split_assembly_line/2 spit line into a list of single word
+split_assembly_line(Line, SplittedLine) :-
+    % Find all whitespace
+    findall(X, char_type(X, white), White), member(X, White),
+    % Split string using whitespaces
+    split_string(Line, X, X, SplittedLine).
 
 /*
-skip_whitespaces([C | Cs], MoreInput) :-
-    is_white(C), !, skip_whitespaces(Cs, MoreInput).
-skip_whitespaces([C | Cs], [C | Cs]) :-
-    \+ is_white(C),
-    !.
-
-skip_whitespaces([], []) :- !.
-
-%%% parse_command/2: Parse the command, return a list
-parse_command(Command, CommandList) :-
-    fail.
-*/
 
 %%% assembler/2: Convert assembly program into machine code.
 % Skip blank line
-assembler([Row | RestFile], LinePointer, Mem) :-
-    % Find all whitespace
-    findall(X, char_type(X, white), White), member(X, White),
-    split_string(Row, X, X, [Splitted | _]),
+assembler([Line | RestFile], LinePointer, Mem) :-
+    split_assembly_line(Line, [HeadSplittedLine | _]),
     % If Line is blank skip it
-    Splitted = "", !,
+    HeadSplittedLine = "", !,
 	assembler(RestFile, LinePointer, Mem).
-assembler([Row | RestFile], LinePointer, [MachineCodeLine | Mem]) :-
-    MachineCodeLine is LinePointer,
+% Convert each line into machine code
+assembler([Line | RestFile], LinePointer, [MemLine | Mem]) :-
+    split_assembly_line(Line, SplittedLine),
+    assembler_line(SplittedLine, MemLine), !,
     NewLinePointer is LinePointer + 1,
     assembler(RestFile, NewLinePointer, Mem).
-    /*
-    % Split the ro into a list of strings
-    split_string("a \tb  \t\t  \tu \t\t\t\t\t\t   c  \t   d", "\t ", " \t", L).
 
-    % Get the codes of a single line
-    evaluate_line(File_codes, Line_codes, Rest_file_codes),
-    % Parse the string list and return a list of atoms
-    pasre_line(Line_codes, Line_list),
-    % Convert single line list into machine code
-    line_to_machineCode(Line_list, MachineCode_line),
-    New_row is Row + 1,
-    assembler_to_machineCode(Rest_file_codes, New_row, Mem).
-    */
-
-
+*/
+% If not unify with other, then is a compile error
+assembler([Line | RestFile], LinePointer, [MemLine | Mem]) :-
+    format('COMPILE ERROR: Invalid instruction\nat line ~d: "~s".\n', [LinePointer, Line]),
+    fail.
 /*
 % if no label defined fail
 assembler([], LinePointer, Mem) :-
@@ -155,7 +133,7 @@ assembler([], LinePointer, Mem) :-
 
 % Compile succesfully
 assembler([], LinePointer, []) :-
-    write("Compiled succesfully!").
+    write("Msg: Compiled succesfully."), nl.
 
 
 %%% lmc_load/2: Given a file, return the content of the memory.
@@ -165,10 +143,10 @@ lmc_load(Filename, Mem) :-
     % Convert ouput file string to lowercase
     string_lower(OutputString, OutputStringLower),
     % Split output file string into a list of rows (Windows and linux support)
-    split_string(OutputStringLower, '\n', '\r', RowList),
-    write(RowList),
-    % Convert assembly programm into machine code starting from row 0
-    assembler(RowList, 0, MemUndefined),
+    split_string(OutputStringLower, '\n', '\r', LineList),
+    write(LineList), nl,
+    % Convert assembly programm into machine code starting from line 0
+    assembler(LineList, 0, MemUndefined),
     write(MemUndefined),
     % resolve_label(MemUndefined, Mem),
     close(Input).
